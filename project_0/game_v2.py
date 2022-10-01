9f088063f109076a3f48b2d256d03db8a37f5b31
AntonYurkovskiy <antonyurkovskiy@yandex.ru>
2022-10-01 10:09:55 +0200

    менее 20 попыток

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -20,6 +20,8 @@ def random_predict(number:int=1) -> int:
         predict_number = np.random.randint(1, 101) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
+        #else:
+           #if (number - predict_number)< 10
     return(count)
 
 def score_game(random_predict) -> int:
@@ -33,7 +35,7 @@ def score_game(random_predict) -> int:
     """
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(100)) # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
